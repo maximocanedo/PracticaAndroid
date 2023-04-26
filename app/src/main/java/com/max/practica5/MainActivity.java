0f87ad018b46a54c5b61bdfb7b5946c71b79aa10
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-04-26 08:49:13 -0300

    Resolución práctica 6

--- app/src/main/java/com/max/practica5/MainActivity.java
+++ app/src/main/java/com/max/practica5/MainActivity.java
@@ -3,9 +3,13 @@ package com.max.practica5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,25 +18,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // Controles
+        EditText txtNombre = findViewById(R.id.txtNombre);
+        CheckBox cbEsProgramador = findViewById(R.id.cbEsProgramador);
+        RadioButton radioHombre = findViewById(R.id.radioHombre);
+        RadioButton radioMujer = findViewById(R.id.radioMujer);
+        TextView txtResultado = findViewById(R.id.txtResultado);
         Button btnAceptar = findViewById(R.id.btnAceptar);
-        btnAceptar.setOnClickListener(new View.OnClickListener() {
+
+        txtNombre.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View view) {
-                TextView txtResultado = findViewById(R.id.txtResultado);
-                EditText txtApellido = findViewById(R.id.txtApellido);
-                EditText txtNombre = findViewById(R.id.txtNombre);
-                EditText txtEdad = findViewById(R.id.txtEdad);
-                EditText txtDireccion = findViewById(R.id.txtDireccion);
-                String apellido = txtApellido.getText().toString();
-                String nombre = txtNombre.getText().toString();
-                String edad = txtEdad.getText().toString();
-                String direccion = txtDireccion.getText().toString();
-                String finalMessage = "¡Bienvenido " + nombre + " " + apellido + "! \n"
-                + "Tenés " + edad + " años. \nVivís en: " + direccion + ". ";
-                txtResultado.setText(finalMessage);
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Ocurre antes de que el texto se cambie.
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Justo cuando se cambia el texto
+                boolean estaVacio = (charSequence.toString().trim().length() == 0);
+                btnAceptar.setEnabled(!estaVacio);
+                txtNombre.setError((!estaVacio) ? null : "Completá este campo");
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // Luego de que el texto se cambia
             }
         });
 
+        btnAceptar.setOnClickListener(view -> {
+            String salida = "";
+            salida += "Nombre: " + txtNombre.getText().toString() + "\n";
+            salida += "Ocupación: " + (String)(cbEsProgramador.isChecked()?"Programador":"No es programador") + "\n";
+            salida += "Sexo: " + (String)(radioHombre.isChecked()?"Hombre":"Mujer") + "\n";
+            txtResultado.setText(salida);
+        });
     }
 }
